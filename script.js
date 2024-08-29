15b0918cd34a38d4d1f7630a36b4e64b298fdb23
Matt Roberts <mroberts0884@gmail.com>
2024-08-29 00:25:46 -0400

    add functionality for slide bar

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 var slider = document.getElementById("myRange");
 var output = document.getElementById("monthly-price");
-output.innerHTML = "$" + slider.value + "/month"; // Display the default slider value
+var views = document.getElementById("views");
+var pageViewDisplay = document.getElementById("pageViewDisplay");
 
-// Update the current slider value (each time you drag the slider handle)
+// Display the default slider value
+output.innerHTML = "$" + slider.value + " /month";
+pageViewDisplay.innerHTML = calculatePageViews(slider.value);
+
+// Update the current slider value and corresponding views
 slider.oninput = function() {
-  output.innerHTML = "$" + this.value + " /month"; 
-  
+    var paymentValue = this.value
+    output.innerHTML = "$" + paymentValue + " /month"; 
+    var viewsValue = calculatePageViews(paymentValue)
+    views.value = viewsValue;
+    pageViewDisplay.innerHTML = viewsValue; // Show the value in the span
 }
-slider.style.backgroundColor = "yellow";
+
+function calculatePageViews(paymentValue) {
+    if (paymentValue >= 8 && paymentValue < 12) {
+        return 10000
+    }
+    else if (paymentValue >= 12 && paymentValue < 16) {
+        return 50000
+    }
+    else if (paymentValue >= 16 && paymentValue < 24) {
+        return 100000 
+    }
+    else if (paymentValue >= 24 && paymentValue < 36) {
+        return 100000 
+    }
+    else if (paymentValue == 36) {
+        return 1000000
+    }
+
+}
+
